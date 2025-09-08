95d1ab137cab2089cb24aa55a8c05efcbb20b4ef
Zolstoy <zolstoy.dev@gmail.com>
2025-09-08 08:07:58 +0200

    WIP

--- src/main.cpp
+++ src/main.cpp
@@ -12,16 +12,60 @@
 
 typedef websocketpp::server<websocketpp::config::asio> server;
 
-using websocketpp::lib::bind;
-using websocketpp::lib::placeholders::_1;
-using websocketpp::lib::placeholders::_2;
-
-// pull out the type of messages sent by our config
 typedef server::message_ptr message_ptr;
 
-// Define a callback to handle incoming messages
+typedef std::map<websocketpp::connection_hdl, std::shared_ptr<class client>,
+                 std::owner_less<websocketpp::connection_hdl>>
+    client_container;
+
+class client
+{
+    server&                             server_;
+    websocketpp::connection_hdl         hdl_;
+    std::mutex&                         server_mtx_;
+    std::shared_ptr<asio::steady_timer> timer_;
+
+   public:
+    client(server& s, websocketpp::connection_hdl hdl, std::mutex& mtx)
+        : server_(s)
+        , hdl_(hdl)
+        , server_mtx_(mtx)
+    {}
+
+    void on_tick(const asio::error_code& error)
+    {
+        if (error)
+        {
+            std::cout << "Timer error: " << error.message() << std::endl;
+            return;
+        }
+
+        nlohmann::json j;
+        nlohmann::json jin;
+        jin["position"] = {
+            static_cast<double>(rand() % 100) / 10.0,
+            0.0,
+            static_cast<double>(rand() % 100) / 10.0,
+        };
+        j["PlayerState"] = jin;
+
+        std::string welcome_message = j.dump();
+        {
+            std::lock_guard<std::mutex> lock(server_mtx_);
+            server_.send(hdl_, welcome_message, websocketpp::frame::opcode::text);
+        }
+        wait();
+    }
+
+    void wait()
+    {
+        timer_->expires_at(std::chrono::steady_clock::now() + std::chrono::seconds(1));
+        timer_->async_wait(bind(&client::on_tick, this, std::placeholders::_1));
+    }
+};
+
 void
-on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg)
+on_message(client_container clients, std::mutex& mtx, websocketpp::connection_hdl hdl, message_ptr msg)
 {
     std::cout << "on_message called with hdl: " << hdl.lock().get() << " and message: " << msg->get_payload()
               << std::endl;
@@ -32,7 +76,7 @@ on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg)
 
     try
     {
-        s->send(hdl, msg->get_payload(), msg->get_opcode());
+        // server_->send(hdl, msg->get_payload(), msg->get_opcode());
     } catch (websocketpp::exception const& e)
     {
         std::cout << "Echo failed because: "
@@ -40,61 +84,42 @@ on_message(server* s, websocketpp::connection_hdl hdl, message_ptr msg)
     }
 }
 
-void
-handler(std::mutex& mtx, server& server, websocketpp::connection_hdl hdl, std::shared_ptr<asio::steady_timer> timer,
-        const asio::error_code& error)
-{
-    if (error)
-    {
-        std::cout << "Timer error: " << error.message() << std::endl;
-        return;
-    }
-
-    nlohmann::json j;
-    nlohmann::json jin;
-    jin["position"] = {
-        static_cast<double>(rand() % 100) / 10.0,
-        0.0,
-        static_cast<double>(rand() % 100) / 10.0,
-    };
-    j["PlayerState"] = jin;
-
-    std::string welcome_message = j.dump();
-    {
-        std::lock_guard<std::mutex> lock(mtx);
-        server.send(hdl, welcome_message, websocketpp::frame::opcode::text);
-    }
-    timer->expires_at(timer->expiry() + std::chrono::seconds(1));
-    timer->async_wait(bind(&handler, std::ref(mtx), std::ref(server), hdl, timer, std::placeholders::_1));
-}
-
 int
 main()
 {
-    std::mutex mtx;
-    server     echo_server;
+    std::mutex       mtx;
+    server           echo_server;
+    client_container clients;
 
     try
     {
 #ifndef NDEBUG
-        echo_server.set_access_channels(websocketpp::log::alevel::none);
+        echo_server.set_access_channels(websocketpp::log::alevel::all);
 #endif
         echo_server.clear_access_channels(websocketpp::log::alevel::frame_payload);
 
-        echo_server.set_error_channels(websocketpp::log::elevel::none);
+        echo_server.set_error_channels(websocketpp::log::elevel::all);
 
         echo_server.init_asio();
 
         echo_server.set_open_handler([&](websocketpp::connection_hdl hdl) {
             std::cout << "Client connected: " << hdl.lock().get() << std::endl;
 
-            std::shared_ptr<asio::steady_timer> timer = std::make_shared<asio::steady_timer>(
-                echo_server.get_io_service(), std::chrono::steady_clock::now() + std::chrono::seconds(1));
+            auto client_p =
+                clients.insert(std::make_pair(hdl, std::make_shared<client>(echo_server, hdl, mtx))).first->second;
 
-            timer->async_wait(
-                std::bind(&handler, std::ref(mtx), std::ref(echo_server), hdl, timer, std::placeholders::_1));
+            client_p->wait();
         });
-        echo_server.set_message_handler(bind(&on_message, &echo_server, ::_1, ::_2));
+
+        echo_server.set_close_handler([&](websocketpp::connection_hdl hdl) {
+            std::cout << "Client disconnected: " << hdl.lock().get() << std::endl;
+
+            std::lock_guard<std::mutex> lock(mtx);
+            clients.erase(hdl);
+        });
+
+        echo_server.set_message_handler(
+            std::bind(&on_message, std::ref(clients), std::ref(mtx), std::placeholders::_1, std::placeholders::_2));
 
         echo_server.listen(9002);
 
